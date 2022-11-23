1dd8b65cf4871612857e1465b0f115131e295cc9
Janira Crispin <janiracrispin@gmail.com>
2022-11-23 15:30:02 -0500

    Exponentation Problems

--- Recursion_Exercises.rb
+++ Recursion_Exercises.rb
@@ -7,7 +7,7 @@ def range(start, last) # 1 4
     range(start, last - 1) << last - 1#1, 4
 end 
 
-p range(1, 5)
+# p range(1, 5)
 
 def sum(array)
     sum = 0
@@ -17,7 +17,7 @@ def sum(array)
     return sum
 end
 
-puts sum([1,2,3,4])
+# puts sum([1,2,3,4])
 
 # Base case: rec_sum([]) = 0
              #rec_sum([1]) = 1
@@ -28,7 +28,34 @@ def rec_sum(array)
    array[0] + rec_sum(array[1..-1])
 end
 
-p rec_sum([]) #=> 0
-p rec_sum([1]) #=> 1
-p rec_sum([1,2]) #=> 3
-p rec_sum([1,2,3]) #=> 6
+# p rec_sum([]) #=> 0
+# p rec_sum([1]) #=> 1
+# p rec_sum([1,2]) #=> 3
+# p rec_sum([1,2,3]) #=> 6
+
+#Base Case: if exponent == 0 return 1
+#Repeating method: base * rec1(base, exponent - 1)
+def rec1(base, exponent)
+    return 1 if exponent == 0
+    base * rec1(base, exponent - 1)
+end 
+
+# p rec1(3, 2)
+
+#Base Case: if exponent == 0 return 1
+#Base Case: if exponent == 1 return base
+#Repeating method: 
+def rec2(base, exponent)
+    return 1 if exponent == 0
+    return base if exponent == 1
+
+    if exponent.even? 
+        rec2(base, exponent / 2) * rec2(base, exponent / 2)
+    else
+        base * ((rec2(base, (exponent - 1) / 2) * (rec2(base, (exponent - 1) / 2)))) 
+    end 
+end 
+
+# p rec2(3, 3)
+# p rec2(3, 4)
+# p rec2(3, 5)
