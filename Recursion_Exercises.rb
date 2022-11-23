551fead0f15920279b6c68b3158bdfd10f96cb0a
Daniel Lee <dlee13195@gmail.com>
2022-11-23 14:55:39 -0500

    rec_sum practice

--- Recursion_Exercises.rb
+++ Recursion_Exercises.rb
@@ -8,3 +8,27 @@ def range(start, last) # 1 4
 end 
 
 p range(1, 5)
+
+def sum(array)
+    sum = 0
+    array.each do |num|
+         sum += num
+    end
+    return sum
+end
+
+puts sum([1,2,3,4])
+
+# Base case: rec_sum([]) = 0
+             #rec_sum([1]) = 1
+             #rec_sum([1,2]) = 3
+             #rec_sum([1,2,3]) = 6
+def rec_sum(array)
+    return 0 if array.length == 0
+   array[0] + rec_sum(array[1..-1])
+end
+
+p rec_sum([]) #=> 0
+p rec_sum([1]) #=> 1
+p rec_sum([1,2]) #=> 3
+p rec_sum([1,2,3]) #=> 6
